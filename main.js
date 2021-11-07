39762def797aa64d4e7c1ca9616f1dba182fbd79
samwallace949 <samwallace949@gmail.com>
2021-11-06 22:22:33 -0400

    changed url to deployment target

--- main.js
+++ main.js
@@ -42,8 +42,8 @@ let users = {
 let userID = 0;
 let currentUser = users[userID];
 
-const url = "http://localhost:3000";
-//const url  = "https://shielded-spire-81354.herokuapp.com/"
+//const url = "http://localhost:3000";
+const url  = "https://shielded-spire-81354.herokuapp.com/"
 async function fetchDefaultReturn(url, params){
     return await fetch(url, params)
     .then(async res => {
